b77db5d563803a30f93b1fc2c76329fce27a5734
Jean-Francois Denise <jdenise@redhat.com>
2024-08-30 18:01:51 +0200

    Providers for eap 8.1 and xp 6.0

---
+++ eap-8.1/src/main/java/org/jboss/eap/glow/metadata/MetadataProvider81.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.eap.glow.metadata;
+
+/**
+ *
+ * @author jdenise
+ */
+public class MetadataProvider81 extends MetadataProviderImpl {
+
+    public MetadataProvider81() {
+        super("eap-8.1", "eap-glow-metadata-8.1");
+    }
+}
---
+++ eap-xp-6.0/src/main/java/org/jboss/eap/glow/metadata/MetadataProviderXP60.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.eap.glow.metadata;
+
+/**
+ *
+ * @author jdenise
+ */
+public class MetadataProviderXP60 extends MetadataProviderImpl {
+
+    public MetadataProviderXP60() {
+        super("eap-xp-6.0", "eap-xp-glow-metadata-6.0");
+    }
+}
