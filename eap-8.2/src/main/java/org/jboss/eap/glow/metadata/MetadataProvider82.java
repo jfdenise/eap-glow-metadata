4e6beb6b75d14fa8a2904d95b147a9dc91507485
Jean-Francois Denise <jfdenise@gmail.com>
2026-01-27 16:04:42 +0100

    metadata for 8.2

--- eap-8.2/src/main/java/org/jboss/eap/glow/metadata/MetadataProvider82.java
+++ eap-8.2/src/main/java/org/jboss/eap/glow/metadata/MetadataProvider82.java
@@ -8,9 +8,9 @@ package org.jboss.eap.glow.metadata;
  *
  * @author jdenise
  */
-public class MetadataProvider81 extends MetadataProviderImpl {
+public class MetadataProvider82 extends MetadataProviderImpl {
 
-    public MetadataProvider81() {
-        super("eap-8.1", "eap-glow-metadata");
+    public MetadataProvider82() {
+        super("eap-8.2", "eap-glow-metadata");
     }
 }
