a105fa4e9ec76b0f691bc832ebb3f6b2a945142f
Jean-Francois Denise <jfdenise@gmail.com>
2026-03-17 18:45:15 +0100

    Include layer configuration in metadata

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# A script to download the layers config locally and package them in the metadata artifact
+
+targetPath=eap-8.2/src/main/resources/eap-glow-metadata/layers-configuration
+mkdir -p $targetPath
+
+# Generate the sources.
+
+cd layers-config-gen;mvn clean install;cd ..
+cp -r layers-config-gen/target/metadata/layers-configuration/* $targetPath
+
+# Build the metadata
+cd eap-8.2;mvn clean install;cd ..
+echo "Done, check that new files have been added, use git to add them"
